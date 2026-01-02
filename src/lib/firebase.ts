18ee4e12fbcc3bb5429b44bbd7022bfbcf909620
skylineontheway <skylineontheway@hotmail.com>
2026-01-02 17:49:32 +0300

    feat: Add Firebase initialization and example environment variables, and update gitignore to properly exclude environment files.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCUaj4Wf1Vhq71SZKTZi0JUhG8TcRefxhs",
-    authDomain: "abdoocoderportfolio.firebaseapp.com",
-    projectId: "abdoocoderportfolio",
-    storageBucket: "abdoocoderportfolio.firebasestorage.app",
-    messagingSenderId: "403038742218",
-    appId: "1:403038742218:web:db3aaeb0e6e826d3863633",
-    measurementId: "G-LESL4N1GCG"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
